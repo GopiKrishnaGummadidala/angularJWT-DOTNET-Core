0a99acba1318e5ad001c9e44b345a4fba2a658dd
Gopi <gopikrishna.gummadidala@gmail.com>
2022-02-13 19:44:47 +0100

    1. added user component for CRUD 2. primeng installed 3. added LiteDB configuration path

--- angularJWT-DOTNET-Core/angularJWT-DOTNET-Core/Rhenus.AngularJWTDOTNETCore.Web/ClientApp/src/app/app.module.ts
+++ angularJWT-DOTNET-Core/angularJWT-DOTNET-Core/Rhenus.AngularJWTDOTNETCore.Web/ClientApp/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { CounterComponent } from "./counter/counter.component";
+import { FetchDataComponent } from "./fetch-data/fetch-data.component";
+import { UserComponent } from "./user/user.component";
+import { TableModule } from "primeng-lts/table";
 
 @NgModule({
   declarations: [
@@ -16,19 +18,26 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    UserComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent },
-], { relativeLinkResolution: 'legacy' })
+    TableModule,
+    RouterModule.forRoot(
+      [
+        { path: "", component: HomeComponent, pathMatch: "full" },
+        { path: "user", component: UserComponent },
+        { path: "counter", component: CounterComponent },
+        { path: "fetch-data", component: FetchDataComponent },
+      ],
+      { relativeLinkResolution: "legacy" }
+    ),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angularJWT-DOTNET-Core/angularJWT-DOTNET-Core/Rhenus.AngularJWTDOTNETCore.Web/ClientApp/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+
+export interface Car {
+  vin;
+  year;
+  brand;
+  color;
+}
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
+})
+export class UserComponent implements OnInit {
+  cars: Car[];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.cars.push({ vin: "123", year: "2020", brand: "BMW", color: "Black" });
+  }
+}
